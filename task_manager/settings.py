b253aa70b0018f307decc1991aba9d9b5affe66d
bogdan <telioko@tutanota.com>
2024-02-21 13:19:47 +0300

    update setting.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
+if DEBUG == 'RENDER':
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
